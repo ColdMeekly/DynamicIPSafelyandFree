a9ab9c36d4136b2ba2d9c52ab16d2b2c918b6f20
Unknown <andrejtar@gmail.com>
2018-01-28 22:26:27 +0000

    Added more validation and cleaned up
    Taking into consideration more variables, still not fully there yet.

--- NodeJS/TCP-Express-Request/Server/app.js
+++ NodeJS/TCP-Express-Request/Server/app.js
@@ -6,16 +6,37 @@ var sslOptions = {
   cert: fs.readFileSync('cert.cert')
 };
 var server = express();
-const approvedDevices = ['ColdMeekly#DESKTOP-4SS8KL0', 'CUSTOM_LP_VIT'];
+const approvedDevices = ['DESKTOP-4SS8KL0', 'CUSTOM_LP_VIT'];
 const basicAuth = require('express-basic-auth')
+const SUCCESS_CODE = 204;
+
+function getFormattedTime() {
+  return `[${("0" + date.getHours()).slice(-2)}:${("0" + date.getMinutes()).slice(-2)}:${("0" + date.getSeconds()).slice(-2)}]`;
+}
 
 function reply(message) {
 	date = new Date();
 	console.log(`\x1b[33m[${("0" + date.getHours()).slice(-2)}:${("0" + date.getMinutes()).slice(-2)}:${("0" + date.getSeconds()).slice(-2)}]\x1b[0m	${message}`);
 }
 
-function replyToUser(message) {
-	console.log(`\x1b[36m$: \x1b[0m${message}`);
+function isValidUserAgent(useragent) {
+  // Wanted format = ColdMeekly#DESKTOP
+  return (
+    userAgent.length > 0 &&
+    userAgent.indexOf('#') > 0 &&
+    userAgent.split('#').length == 2
+  )
+}
+
+// Will be using 401 because that's what basicAuth denies with.
+function declineAccess(res) {
+  res.status(401).end();
+  return;
+}
+
+function acceptAccess(res) {
+  res.status(SUCCESS_CODE).end();
+  return;
 }
 
 https.createServer(sslOptions, server).listen(443)
@@ -29,16 +50,22 @@ server.use(basicAuth(
 
 server.get('/updater',function(req, res){
   let userAgent = req.get('User-Agent');
-  let splitAgent = userAgent.split("#");
-  // ColdMeekly#DESKTOP => ["ColdMeekly","DESKTOP"]
-  //                        username      ComputerName
 
-  if (approvedDevices.indexOf(userAgent) >= 0)
+  // Make sure that the user agent is in the format that we expect
+  if (!isValidUserAgent(userAgent)) {
+    declineAccess(res);
+    return;
+  }
+
+  let splitAgent = userAgent.split("#");
+
+  if (approvedDevices.indexOf(splitAgent[1]) > 0)
   {
-    res.status(204).end();
+    acceptAccess(res);
 
 
     reply(`Got IP for '${splitAgent[0]} on ${splitAgent[1]}' => ${req.connection.remoteAddress}`);
+
     // Create folder structure
     if (!fs.existsSync('Logs')){
         fs.mkdirSync('Logs');
@@ -46,15 +73,23 @@ server.get('/updater',function(req, res){
     if (!fs.existsSync(`Logs/${splitAgent[0]}`)){
         fs.mkdirSync(`Logs/${splitAgent[0]}`);
     }
+    //
+
+    // Write IP to file
     fs.writeFile(`Logs/${splitAgent[0]}/${splitAgent[1]}.txt`, req.connection.remoteAddress, (err) => {
-      // throws an error, you could also catch it here
       if (err) {
         reply(`File write error: ${err}`);
       }
   });
   }
   else {
-    res.status(401).end();
+    // UserAgent not in our list.
+    declineAccess(res);
+    if (!fs.existsSync('Logs')){
+        fs.mkdirSync('Logs');
+    }
+    reply(`Rejected unauthorized identifier [${userAgent}]`);
+    fs.appendFileSync('Logs/AuthLogs.txt', `${getFormattedTime()} UNKNOWN_ID ${userAgent}`);
   }
 
 });
